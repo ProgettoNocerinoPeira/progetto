e8577fad7ebb34f38f0cfb43d734b3f13e761e51
matusow <appalla94@gmail.com>
2017-01-27 15:30:12 +0100

    Add files via upload

---
+++ c.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char* argv[]){
+
+  printf("\n\n\n c=%d\n\n\n",atoi(argv[1]));
+
+}
---
+++ cronometro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+	printf("\nSono il cronometro \n");
+
+	
+	return 0;
+}
