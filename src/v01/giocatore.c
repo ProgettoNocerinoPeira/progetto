46c3f74c075a064f06f3bc0e9cc5d9aa5f8618aa
matusow <appalla94@gmail.com>
2017-01-29 16:46:56 +0100

    Update giocatore.c
    add function tiro (nn testata dubbio su sigusr1)

--- src/v01/giocatore.c
+++ src/v01/giocatore.c
@@ -14,7 +14,23 @@
 
 //Include our commonKeys header file
 #include "commonKeys.h"
+//Protitype our functions
+int tiro(int goal,int team);
+
+//From here we start writing our functions
+int tiro(int goal, int team){
+  if ((goal==1)&&(team==1)){
+    //goal team 1
+    SIGUSR1;
+  }else{
+    //goal team 2
+    SIGUSR2;
+  }
+  
+}
+
 
 int main (int argc, char *argv[]){
   int myteam=atoi(argv[1]);
+  int team=atoi(argv[2]);
 }
