35d1725dad57bde4b4405f8cfb7c49a959424cec
matusow <appalla94@gmail.com>
2017-01-29 15:43:41 +0100

    Update giocatore.c

---
+++ src/v01/giocatore.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
+
+//Include our commonKeys header file
+#include "commonKeys.h"
+
+int main (int argc, char *argv[]){
+  int myteam=atoi(argv[1]);
+}
