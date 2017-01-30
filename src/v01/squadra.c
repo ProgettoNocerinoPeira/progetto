4b6c24ee7548d2ff77c04f7b0ad32c0719daaf11
Mattia Nocerino <mnocerino@gmail.com>
2017-01-30 11:18:10 +0100

    test

--- src/v01/squadra.c
+++ src/v01/squadra.c
@@ -74,6 +74,7 @@ int main (int argc, char *argv[]){
 
   while (1){
     int semValue = semctl(semaphoreSetId,teamNumber, GETVAL, semaphore);
+    printf("Semaforo team: %d, valore: %d", teamNumber, semaphore.val);
     while(semaphore.val>0 && semaphore.val<=5){
       //TODO Spawn players
       semaphore.val=semaphore.val-1;
