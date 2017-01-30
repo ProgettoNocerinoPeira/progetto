6d97601aca54052a9459e86bbe19675f1538d8be
Mattia Nocerino <mnocerino@gmail.com>
2017-01-30 16:34:37 +0100

    test

--- src/v01/fato.c
+++ src/v01/fato.c
@@ -22,13 +22,13 @@ int teamNumber;
 char msglog [256];
 
 
-struct mymsg 
+struct mymsg
 {
-	int  mtype;	/* Message type */
-        int  mtext;     /* Message body */
+  int  mtype;	/* Message type */
+  int  mtext;     /* Message body */
 } msg;
 
-
+int team,type;
 int createMessageQueue();
 void writeLog(char *message);
 int generateRandom(int value);
@@ -42,24 +42,26 @@ int createMessageQueue(){
   return messageQueue;
 }
 
-int readMessage(){
-  //restituisco il type in caso di successo
-   msgrcv(messageQueueId,&msg,sizeof(msg), 0,0);
-   if(generateRandom(30)==1){
-     return (msg.mtype);
-     teamNumber=msg.mtext;
-     //modifico i parametri e rispondo vero
-     msg.mtype=4;
-      msg.mtext=1;
-      msgsnd(messageQueueId, &msg, sizeof(msg),0);
-   }else{
-     //modifico i parametri e rispondo falso
-     msg.mtype=4;
-     msg.mtext=0;
-     msgsnd(messageQueueId, &msg, sizeof(msg),0);
-   }
-   
-  //leggo il numero della squadra
+
+int readAndAnswerMessage(){
+  msgrcv(messageQueueId, &msg, sizeof(msg), 0, 0);
+  teamNumber = msg.mtext;
+  type = msg.mtype;
+  if (msg.mtype==1){
+    if (generateRandom(30)==1) msg.mtext=1;
+  }
+  if (msg.mtype==2){
+    if (generateRandom(30)==1) msg.mtext=1;
+  }
+  if (msg.mtype==3){
+    if (generateRandom(30)==1) msg.mtext=1;
+  }
+  else msg.text=0;
+  msg.mtype=4;
+  msgsnd(messageQueueId, &msg, sizeof(msg),0);
+  return msg.mtext;
+}
+//leggo il numero della squadra
 }
 
 void writeLog(char* text){
@@ -75,17 +77,17 @@ void writeLog(char* text){
     printf("\n");
     fclose(logFile);
   }
-else printf("Errore nel file di log!!!\n");
+  else printf("Errore nel file di log!!!\n");
 }
 //funzione che randomizza in base al tempo  preso in input un parametro restituisce 0/1
 int generateRandom(int value){
-   srand(time(NULL));
-   sleep(1);//per cambiare il risultato ad ogni accesso
-   if (value>rand()%100){
-     return 0;
-   }else{
-     return 1;
-   }
+  srand(time(NULL));
+  sleep(1);//per cambiare il risultato ad ogni accesso
+  if (value>rand()%100){
+    return 0;
+  }else{
+    return 1;
+  }
 }
 
 int main(int argc, char *argv[]){
@@ -93,28 +95,21 @@ int main(int argc, char *argv[]){
   messageQueueId=createMessageQueue();
   if ((messageQueueId==-1)) writeLog("Failed to create/attach to messageQueue");
   while(1){
-  
-    if (readMessage()==1){
-     //tiro a segno
-     msglog [256]="La squadra ";
-     sprintf(msglog,"%d ha fatto GOAL",teamNumber);
-      writeLog (msglog);
-   }
-    if (readMessage()==2){
-     //infortunio
-     msglog [256]="Il giocatore della squadra ";
-     sprintf(msglog,"%d si è infortunato",teamNumber);
-     writeLog (msglog);
-   }
-    if (readMessage()==3){
-     //dribilg
-     msglog [256]="Il giocatore della squadra ";
-     sprintf(msglog,"%d ha vinto il contrasto",teamNumber);
-     writeLog (msglog);
-   }
+    int response = readAndAnswerMessage();
+    if (msg.mtext==1){
+      if (response==1){
+        sprintf(msglog, "La squadra %d ha fatto Goal.", teamNumber);
+        writeLog (msglog);
+      }
+      if (response==2){
+        sprintf(msglog, "Il giocatore della squadra %d ha subito un infortunio.", teamNumber);
+        writeLog (msglog);
+      }
+      if (response==3){
+        sprintf(msglog, "Il giocatore della squadra %d ha vinto il dribbling.", teamNumber);
+        writeLog (msglog);
+      }
+    }
   }
-
-
   return 0;
-
 }
