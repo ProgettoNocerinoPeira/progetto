47cf341642aa7f47d5b89f56fbe631cd92e66aa2
Mattia Nocerino <mnocerino@gmail.com>
2017-01-30 13:14:45 +0100

    test

--- src/v01/squadra.c
+++ src/v01/squadra.c
@@ -15,29 +15,14 @@
 //Include our commonKeys header file
 #include "commonKeys.h"
 
-
-union semun {
-  // value for SETVAL
-  int val;
-  // buffer for IPC_STAT, IPC_SET
-  struct semid_ds* buf;
-  // array for GETALL, SETALL
-  unsigned short* array;
-  // Linux specific part
-  #if defined(__linux__)
-  // buffer for IPC_INFO
-  struct seminfo* __buf;
-  #endif
-} semaphore, ballSemaphore;
-struct sembuf buffer;
-
-
-
+struct sembuf ops;
 int semaphoreSetId, teamNumber;
+
+
 int connectToBall();
 bool spawn();
 void sig_handler(int signo);
-
+void decreaseSemaphore();
 
 void sig_handler(int signo){
   if (signo == SIGINT){
@@ -66,25 +51,21 @@ int connectToSemaphore(){
   semaphoreId=semget(semaphoreKey, 3, IPC_CREAT | 0666);
   return semaphoreId;
 }
+void decreaseSemaphore(){
+  ops.sem_num=teamNumber;
+  ops.sem_op=-1;
+  ops.sem_flg = 0;
+  semop(semaphoreSetId, ops, 1);
+}
 int main (int argc, char *argv[]){
 
   teamNumber = atoi(argv[1]);
   signal(SIGINT, sig_handler);
   semaphoreSetId=connectToSemaphore();
-  printf("Semaforo squadra %d, id: %d\n", teamNumber,semaphoreSetId);
-  semaphore.val=5;
-  semctl(semaphoreSetId,teamNumber, SETVAL, semaphore);
+  semctl(semaphoreSetId,teamNumber, SETVAL, 5);
   while (1){
-    int semValue = semctl(semaphoreSetId,teamNumber, GETVAL, semaphore);
-    printf("Semaforo team: %d, valore: %d\n", teamNumber, semaphore.val);
-    sleep(1);
-
-    while(semaphore.val>=0 && semaphore.val<=5){
-      //TODO Spawn players
-      semaphore.val=semaphore.val-1;
-      semctl(semaphoreSetId,teamNumber, SETVAL, semaphore);
-      printf("Team: %d ,numero giocatori: %d\n", teamNumber,(semaphore.val));
-      spawn(teamNumber);
-    }
+    decreaseSemaphore();
+    printf("Team: %d ,numero giocatori: %d\n", teamNumber,semctl(semaphoreSetId,teamNumber,GETVAL));
+    spawn(teamNumber);
   }
 }
