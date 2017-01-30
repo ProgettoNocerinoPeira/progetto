b43364385d8410613fb0d1f2f4512de36e62f442
matusow <appalla94@gmail.com>
2017-01-30 16:13:44 +0100

    Update fato.c
    implemte

--- src/v01/fato.c
+++ src/v01/fato.c
@@ -1,3 +1,4 @@
+
 //Libraries includes
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,16 +18,48 @@
 #include "commonKeys.h"
 //Global variables needed for this piece of software
 int messageQueueId;
+int teamNumber;
+char msglog [256];
+
+
+struct mymsg 
+{
+	int  mtype;	/* Message type */
+        int  mtext;     /* Message body */
+} msg;
+
 
 int createMessageQueue();
 void writeLog(char *message);
 int generateRandom(int value);
+int readMessage();
+
 
 int createMessageQueue(){
   int messageQueue;
   key_t messageKey = KEYMESSAGEQUEUE;
   messageQueue=msgget(messageKey, IPC_CREAT | 0666);
   return messageQueue;
+}
+
+int readMessage(){
+  //restituisco il type in caso di successo
+   msgrcv(messageQueueId,&msg,sizeof(msg), 0,0);
+   if(generateRandom(30)==1){
+     return (msg.mtype);
+     teamNumber=msg.mtext;
+     //modifico i parametri e rispondo vero
+     msg.mtype=4;
+      msg.mtext=1;
+      msgsnd(messageQueueId, &msg, sizeof(msg),0);
+   }else{
+     //modifico i parametri e rispondo falso
+     msg.mtype=4;
+     msg.mtext=0;
+     msgsnd(messageQueueId, &msg, sizeof(msg),0);
+   }
+   
+  //leggo il numero della squadra
 }
 
 void writeLog(char* text){
@@ -59,6 +92,28 @@ int main(int argc, char *argv[]){
   writeLog("Testing the log writer");
   messageQueueId=createMessageQueue();
   if ((messageQueueId==-1)) writeLog("Failed to create/attach to messageQueue");
+  while(1){
+  
+    if (readMessage()==1){
+     //tiro a segno
+     msglog [256]="La squadra ";
+     sprintf(msglog,"%d ha fatto GOAL",teamNumber);
+      writeLog (msglog);
+   }
+    if (readMessage()==2){
+     //infortunio
+     msglog [256]="Il giocatore della squadra ";
+     sprintf(msglog,"%d si è infortunato",teamNumber);
+     writeLog (msglog);
+   }
+    if (readMessage()==3){
+     //dribilg
+     msglog [256]="Il giocatore della squadra ";
+     sprintf(msglog,"%d ha vinto il contrasto",teamNumber);
+     writeLog (msglog);
+   }
+  }
+
 
   return 0;
 
